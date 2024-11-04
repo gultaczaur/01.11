58779a75c70698310914a2c981032e98ae8abcee
gultaczaur <gultaczaur@gmail.com>
2024-11-04 19:05:38 +0400

    Add files via upload

---
+++ GZ-01.11/main.js
@@ -0,0 +1,46 @@
+// let obj = [
+//     {
+//         name: "Ali",
+//         surname: "Aliyev",
+//         age: 10,
+//         driving: true
+//     }, {
+//         name: "Veli",
+//         surname: "Aliyev",
+//         age: 17,
+//         driving: false,
+//     }, {
+//         name: "Mehman",
+//         surname: "Aliyev",
+//         age: 25,
+//         driving: true,
+//     },{
+//         name: "Nicat",
+//         surname: "Aliyev",
+//         age:26,
+//         driving: false
+//     },{
+//         name: "Ayxan",
+//         surname: "Aliyev",
+//         age:15,
+//         driving: true
+//    }
+// ]
+
+// bu arrayde driving (suruculuk vesiqesi) true olanlari ve 
+// yasi 18+ olanlari secib bir arrayde loga cixardacaqsiz 
+// function driveLisence () {
+//     for (let i=0; i<5; i++) {
+//         if  (obj[i].age >= 18 && obj[i].driving == true) {
+//             console.log(obj[i]);
+//             }}}
+// driveLisence ()
+
+
+// soyadi Aliyev olanlari ve yasi 18 den asagi olanlari  secib bir arrayde geri qaytarin
+// function driveLisence () {
+//         for (let i=0; i<5; i++) {
+//             if  (obj[i].surname == "Aliyev" && obj[i].age<18) {
+//                 console.log(obj[i]);
+//                 }}}
+//     driveLisence ()
